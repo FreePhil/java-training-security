f072b3eb05c457dfb4fa424d216afda49ee26a5d
Phil <phil@hengetech.com.tw>
2023-12-20 16:48:15 +0800

    cannot debug

--- src/test/java/com/example/jdbcsecurity/EventAggregateTest.java
+++ src/test/java/com/example/jdbcsecurity/EventAggregateTest.java
@@ -1,12 +1,8 @@
 package com.example.jdbcsecurity;
 
-import com.example.jdbcsecurity.model.Post;
 import com.example.jdbcsecurity.model.futurework.Event;
 import com.example.jdbcsecurity.repository.EventRepository;
-import com.example.jdbcsecurity.repository.PostRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +14,6 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Configuration
 public class EventAggregateTest {
     Logger logger = LoggerFactory.getLogger(EventAggregateTest.class);
 
