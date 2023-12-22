ccf8b738200b6f3381aa58b556bab119943c4150
Phil <phil@hengetech.com.tw>
2023-12-22 17:52:54 +0800

    show some config meta

--- src/main/java/com/example/jdbcsecurity/config/AuditConfig.java
+++ src/main/java/com/example/jdbcsecurity/config/AuditConfig.java
@@ -7,8 +7,22 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 @Configuration
 @EnableJdbcAuditing
 public class AuditConfig {
+    // https://docs.spring.io/spring-data/jdbc/docs/2.2.4/reference/html/#reference
 //    @Bean
 //    AuditorAware<AuditableUser> auditorProvider() {
 //        return new AuditorAwareImpl();
 //    }
 }
+
+//class SpringSecurityAuditorAware implements AuditorAware<User> {
+//
+//    @Override
+//    public Optional<User> getCurrentAuditor() {
+//
+//        return Optional.ofNullable(SecurityContextHolder.getContext())
+//                .map(SecurityContext::getAuthentication)
+//                .filter(Authentication::isAuthenticated)
+//                .map(Authentication::getPrincipal)
+//                .map(User.class::cast);
+//    }
+//}
